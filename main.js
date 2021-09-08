3c847e962ae048b546ef91a61924a95127536a3b
hana-bananaa <debartosiaki@gmail.com>
2021-09-07 21:24:51 -0300

    arrumando postagens criando arquivos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function exibePostagens () {
+function Postagens () {
     let xhr = new XMLHttpRequest
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
     xhr.onload = () => {
@@ -25,4 +25,61 @@ function exibePostagens () {
     }
     xhr.send();
   }
-  exibePostagens()
+  Postagens()
+
+
+  function albuns() {
+    let xhr = new XMLHttpRequest
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/albums')
+    xhr.onload = () => {
+      const posts = JSON.parse(xhr.responseText)
+  
+      for (let i = 0; i < posts.length; i++) {
+        let title = posts[i]['title']
+        let userId = posts[i]['userId']
+        let id = posts[i]['id']
+  
+        let addPosts = document.getElementById('addAlbum')
+  
+        addPosts.innerHTML += 
+        `
+          <tr>
+            <td>${id}</td>
+            <td>${userId}</td>
+            <td>${title}</td>
+          <tr>
+        `
+      }
+    }
+    xhr.send();
+  }
+  albuns()
+
+  function todo(){
+    let xhr = new XMLHttpRequest
+    xhr.open('GET','https://jsonplaceholder.typicode.com/todos')
+    xhr.onload = () => {
+      const posts = JSON.parse(xhr.responseText)
+  
+      for (let i = 0; i < posts.length; i++) {
+        let title = posts[i]['title']
+        let userId = posts[i]['userId']
+        let id = posts[i]['id']
+        let completed = posts[i]['completed']
+  
+        let addPosts = document.getElementById('addtodo')
+  
+        addPosts.innerHTML += 
+        `
+          <tr>
+            <td>${id}</td>
+            <td>${userId}</td>
+            <td>${title}</td>
+            <td>${completed}</td>
+          <tr>
+        `
+      }
+    }
+    xhr.send();
+  }
+  todo()
